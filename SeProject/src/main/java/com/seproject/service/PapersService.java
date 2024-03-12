b3dbcbb4d5a60f0192ca5eed570cfd3938fa3235
lahin03 <148428046+lahin03@users.noreply.github.com>
2024-03-12 14:31:11 +0530

    Update PapersService.java

--- SeProject/src/main/java/com/seproject/service/PapersService.java
+++ SeProject/src/main/java/com/seproject/service/PapersService.java
@@ -1,5 +1,7 @@
 package com.seproject.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +31,9 @@ public class PapersService {
 		return paperRepo.save(paper);
 	}
 	
+    public List<Papers> getAllPapers() {
+        return paperRepo.findAll();
+    }
 
 
 	
